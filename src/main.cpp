4fc273767af5b066ed57864026d97f7b2f9ddca0
WillyBBlue <skihatduckie@gmail.com>
2022-05-30 15:45:52 -0400

    Create sys_mem class in statys.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,17 @@
-#include <windows.h>
-#include <stdio.h>
-#include <tchar.h>
+#include <iostream>
+#include "statys.h"
 
-// Use to convert bytes to KB
-#define DIV 1024
+int main() {
+    statys::sys_mem sys_memory;
 
-// Specify the width of the field in which to print the numbers. 
-// The asterisk in the format specifier "%*I64d" takes an integer 
-// argument and uses it to pad and right justify the number.
-#define WIDTH 7
-
-void print_system_memory() {
-    MEMORYSTATUSEX statex;
-    statex.dwLength = sizeof(statex);
-    GlobalMemoryStatusEx(&statex);
-
-    _tprintf(
-        TEXT("There is  %*ld percent of memory in use.\n"), WIDTH, statex.dwMemoryLoad
-    );
-    _tprintf(
-        TEXT("There are %*I64d total KB of physical memory.\n"), WIDTH, statex.ullTotalPhys/DIV
-    );
-    _tprintf(
-        TEXT("There are %*I64d free  KB of physical memory.\n"), WIDTH, statex.ullAvailPhys/DIV
-    );
-    _tprintf(
-        TEXT("There are %*I64d total KB of paging file.\n"), WIDTH, statex.ullTotalPageFile/DIV
-    );
-    _tprintf(
-        TEXT("There are %*I64d free  KB of paging file.\n"), WIDTH, statex.ullAvailPageFile/DIV
-    );
-    _tprintf(
-        TEXT("There are %*I64d total KB of virtual memory.\n"), WIDTH, statex.ullTotalVirtual/DIV
-    );
-    _tprintf(
-        TEXT("There are %*I64d free  KB of virtual memory.\n"), WIDTH, statex.ullAvailVirtual/DIV
-    );
-    // Show the amount of extended memory available.
-    _tprintf(
-        TEXT("There are %*I64d free  KB of extended memory.\n"), WIDTH, statex.ullAvailExtendedVirtual/DIV
-    );
-}
-
-int main(void) {
-    // Print the total amount of memory being used by the system
-    print_system_memory();
+    while (true) {
+        int *i = new int;
+    
+        if (sys_memory.get_usage_as_percent() > 75) {
+            std::cout << "System memory usage has exceeded 75%. Breaking from loop.\n";
+            break;
+        }
+    }
 
     return 0;
 }
--- src/statys.h
+++ src/statys.h
@@ -1,5 +1,11 @@
+#ifndef STATYS_H  // include guard
+#define STATYS_H
+
 namespace statys {
     class sys_mem {
-        public: int get_usage_as_percent();
+        public:
+            int get_usage_as_percent();
     };
 }
+
+#endif
